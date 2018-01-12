17ba97b6ce7558465aa4940e2e1a15acb0252241
jiyuu-jin <jiyuu.llc@gmail.com>
2018-01-12 10:41:09 -0500

    Success

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,31 @@
-import React from 'react';
+/* eslint-disable no-mixed-operators,no-undef,class-methods-use-this,no-param-reassign,no-multi-assign */
+import React, { Component } from 'react';
 
-export default function Home() {
-  return (
-    <div className="intro">
-      <br />
-      <br />
-      <br />
-      <br />
-      <center>Welcome to the website of Zachary Pelkey</center>
-    </div>
-  );
+
+class Home extends Component {
+
+
+  componentDidMount() {
+    const intro = document.getElementById('intro');
+    setTimeout(this.introAnimateEnter.bind(this, intro), 1000);
+  }
+
+  introAnimateEnter(intro) {
+    intro.style.fontSize = '60px';
+    setTimeout(this.introAnimateExit.bind(this, intro), 3000);
+  }
+
+  introAnimateExit(intro) {
+    intro.style.left = '1500px';
+  }
+
+  render() {
+    return (
+      <div id="intro" className="intro">
+        <div>Welcome to the website of Zachary Pelkey</div>
+      </div>
+    );
+  }
 }
+
+export default Home;
